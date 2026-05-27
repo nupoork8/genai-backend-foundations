e1be52308f16d734a870f3926d35dcdf7ef237d4
nupoork8 <nupoorktech@gmail.com>
2026-05-27 11:00:08 +0530

    Fastapi server

---
+++ day_04_fastapi_server.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+# 1 Initalize Fstapi web engine instance
+app = FastAPI(title="GenAI Ingestion Pipeline")
+
+# 2 create basic home endpoint
+@app.get("/")
+def read_root():
+
+    return{
+        "status":"Online",
+        "framework":"FastApi",
+        "message":"AI Engine server is active"
+    }
+
+# 3. Create a data endpoint to receive external inputs (POST Request)
+@app.post("/api/v1/query")
+def handle_incoming_query(payload: dict):
+    #safely extract incoming query text sent by an external application
+    user_query = payload.get("query", "")
+
+    simulated_ai_response = f"Processed vector match for query: '{user_query}'"
+
+    return{
+        "success":True,
+        "received_data":{
+            "query": user_query
+        },
+        "backend_output": simulated_ai_response
+    }
